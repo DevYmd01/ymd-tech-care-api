775afb4a8e0862096098e7f1aa02ae56a325e53d
Supakorn Muthita <supakorn.m@YMD.local>
2026-01-22 16:49:30 +0700

    create file module :/pr

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { OrgBranchModule } from './master-data/org_branch/org_branch.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { VendorsModule } from './master-data/vendors/vendors.module';
+import { PrModule } from './modules/procurement/pr/pr.module';
+
 
 
 @Module({
-  imports: [OrgBranchModule, PrismaModule, VendorsModule],
+  imports: [OrgBranchModule, PrismaModule, VendorsModule, PrModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/procurement/pr/dto/ceacte-pr.dto.ts
@@ -0,0 +1,70 @@
+import {
+    IsString,
+    IsDateString,
+    IsOptional,
+    IsNumber,
+    IsEnum,
+    Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreatePRDTO {
+
+    // ปกติให้ backend generate
+    @IsOptional()
+    @IsString()
+    pr_no?: string;
+
+    // วันที่สร้าง PR
+    @IsOptional()
+    @IsDateString()
+    pr_date?: string;
+
+    // สาขา (required)
+    @IsNumber()
+    @Type(() => Number)
+    branch_id: number;
+
+    // คลัง (required)
+    @IsNumber()
+    @Type(() => Number)
+    warehouse_id: number;
+
+    // วันที่ต้องการของ
+    @IsOptional()
+    @IsDateString()
+    need_by_date?: string;
+
+    // สถานะ PR (เช่น DRAFT, SUBMITTED, APPROVED)
+    @IsString()
+    status: string;
+
+    // สกุลเงิน (THB, USD)
+    @IsString()
+    currency_code: string;
+
+    // อัตราแลกเปลี่ยน
+    @IsOptional()
+    @IsNumber()
+    @Min(0)
+    @Type(() => Number)
+    exchange_rate?: number;
+
+    // หมายเหตุ
+    @IsOptional()
+    @IsString()
+    remark?: string;
+
+    // ยอดรวม (ควรคำนวณจาก PR line)
+    @IsOptional()
+    @IsNumber()
+    @Min(0)
+    @Type(() => Number)
+    total_amount?: number;
+
+    // เครดิตวัน
+    @IsNumber()
+    @Min(0)
+    @Type(() => Number)
+    payment_term_days: number;
+}
