48bba49cc262f2de04d9beda07c2fdc06a0937d5
Supakorn Muthita <supakorn.m@YMD.local>
2026-01-29 18:07:22 +0700

    update vender group

--- src/modules/master-data/vendor-group/vendor-group.controller.ts
+++ src/modules/master-data/vendor-group/vendor-group.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Param } from '@nestjs/common';
 import { VendorGroupService } from './vendor-group.service';
+import { CreateVendorGroupDTO } from './dto/creacte-gruop.dto';
 
 @Controller('vendor-group')
 export class VendorGroupController {
@@ -8,5 +9,20 @@ export class VendorGroupController {
     @Get()
     findAll() {
         return this.vendorGroupService.findAll();
+    }
+
+    @Get(':vendor_group_id')
+    findOne(@Param('vendor_group_id') vendor_group_id: string) {
+        return this.vendorGroupService.findOne(+vendor_group_id);
+    }
+
+    @Post()
+    create(@Body() createVendorGroupDTO: CreateVendorGroupDTO) {
+        return this.vendorGroupService.create(createVendorGroupDTO);
+    }
+
+    @Put(':vendor_group_id')
+    update(@Param('vendor_group_id') vendor_group_id: string, @Body() updateVendorGroupDTO: CreateVendorGroupDTO) {
+        return this.vendorGroupService.update(+vendor_group_id, updateVendorGroupDTO);
     }
 }
--- src/modules/master-data/vendor-group/vendor-group.service.ts
+++ src/modules/master-data/vendor-group/vendor-group.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateVendorGroupDTO } from './dto/creacte-gruop.dto';
+
 
 @Injectable()
 export class VendorGroupService {
@@ -11,6 +13,65 @@ export class VendorGroupService {
                 vendor_group_id: true,
                 vendor_group_code: true,
                 vendor_group_name: true,
+            }
+        });
+    }
+
+    async findOne(vendor_group_id: number) {
+        return this.prismaService.vendor_group.findUnique({
+            where: {
+                vendor_group_id: vendor_group_id,
+            },
+            select: {
+                vendor_group_id: true,
+                vendor_group_code: true,
+                vendor_group_name: true,
+                vendor_group_nameeng: true,
+                description: true,
+                status: true,
+            }
+        });
+    }
+
+    async create(createVendorGroupDTO: CreateVendorGroupDTO) {
+        return this.prismaService.vendor_group.create({
+            data: {
+                vendor_group_code: createVendorGroupDTO.vendor_group_code,
+                vendor_group_name: createVendorGroupDTO.vendor_group_name,
+                vendor_group_nameeng: createVendorGroupDTO.vendor_group_nameeng,
+                description: createVendorGroupDTO.description,
+                status: createVendorGroupDTO.status || "ACTIVE",
+            },
+            select: {
+                vendor_group_id: true,
+                vendor_group_code: true,
+                vendor_group_name: true,
+                vendor_group_nameeng: true,
+                description: true,
+                status: true,
+            }
+        });
+    }
+
+    async update(vendor_group_id: number, updateVendorGroupDTO: CreateVendorGroupDTO) {
+        return this.prismaService.vendor_group.update({
+            where: {
+                vendor_group_id: vendor_group_id,
+            },
+            data: {
+                vendor_group_code: updateVendorGroupDTO.vendor_group_code,
+                vendor_group_name: updateVendorGroupDTO.vendor_group_name,
+                vendor_group_nameeng: updateVendorGroupDTO.vendor_group_nameeng,
+                description: updateVendorGroupDTO.description,
+                status: updateVendorGroupDTO.status || "ACTIVE",
+            },
+            select: {
+                vendor_group_id: true,
+                vendor_group_code: true,
+                vendor_group_name: true,
+                vendor_group_nameeng: true,
+                description: true,
+                status: true,
             }
         });
     }
