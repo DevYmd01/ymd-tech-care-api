73b544d6eba9672380fa370017f139ee34876159
Supakorn Muthita <supakorn.m@YMD.local>
2026-01-29 17:06:10 +0700

    create item category

--- src/modules/master-data/item-category/item-category.controller.ts
+++ src/modules/master-data/item-category/item-category.controller.ts
@@ -1,4 +1,29 @@
 import { Controller } from '@nestjs/common';
+import { ItemCategoryService } from './item-category.service';
+import { CreateItemCategoryDto } from './dto/create-item-category.dto';
+import { Post, Body, Get, Param, Put } from '@nestjs/common';
 
 @Controller('item-category')
-export class ItemCategoryController {}
+export class ItemCategoryController {
+    constructor(private readonly itemCategoryService: ItemCategoryService) { }
+
+    @Post()
+    create(@Body() createItemCategoryDto: CreateItemCategoryDto) {
+        return this.itemCategoryService.create(createItemCategoryDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.itemCategoryService.findAll();
+    }
+
+    @Get(':item_category_id')
+    findOne(@Param('item_category_id') item_category_id: string) {
+        return this.itemCategoryService.findOne(+item_category_id);
+    }
+
+    @Put(':item_category_id')
+    update(@Param('item_category_id') item_category_id: string, @Body() updateItemCategoryDto: CreateItemCategoryDto) {
+        return this.itemCategoryService.update(+item_category_id, updateItemCategoryDto);
+    }
+}
--- src/modules/master-data/item-category/item-category.service.ts
+++ src/modules/master-data/item-category/item-category.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { CreateItemCategoryDto } from './dto/create-item-category.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ItemCategoryService {}
+export class ItemCategoryService {
+    constructor(private prisma: PrismaService) { }
+
+    async create(dto: CreateItemCategoryDto) {
+        return this.prisma.item_category.create({
+            data: dto,
+        });
+    }
+
+    async findAll() {
+        return this.prisma.item_category.findMany({
+            where: {
+                is_active: true,
+            },
+        });
+    }
+
+    async findOne(id: number) {
+        return this.prisma.item_category.findUnique({
+            where: {
+                item_category_id: id,
+            },
+        });
+    }
+
+    async update(id: number, dto: CreateItemCategoryDto) {
+        return this.prisma.item_category.update({
+            where: {
+                item_category_id: id,
+            },
+            data: dto,
+        });
+    }
+}
