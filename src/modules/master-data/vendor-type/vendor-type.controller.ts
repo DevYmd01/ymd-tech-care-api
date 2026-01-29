5ad7135d8cd9156885d33cc3f623b2f526fbac73
Supakorn Muthita <supakorn.m@YMD.local>
2026-01-29 15:52:01 +0700

    update vender-type

--- src/modules/master-data/vendor-type/vendor-type.controller.ts
+++ src/modules/master-data/vendor-type/vendor-type.controller.ts
@@ -4,6 +4,9 @@ import { VendorTypeService } from './vendor-type.service';
 import { CreateVendorTypeDTO } from './dto/create-vendor-type.dto';
 import { Post } from '@nestjs/common';
 import { Body } from '@nestjs/common';
+import { Put } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { UpdateVendorTypeDTO } from './dto/update-vendor-type.dto';
 
 @Controller('vendor-type')
 export class VendorTypeController {
@@ -18,5 +21,10 @@ export class VendorTypeController {
     @Post()
     create(@Body() createVendorTypeDTO: CreateVendorTypeDTO) {
         return this.vendorTypeService.create(createVendorTypeDTO);
+    }
+
+    @Put(':vendor_type_id')
+    update(@Param('vendor_type_id') vendor_type_id: string, @Body() updateVendorTypeDTO: UpdateVendorTypeDTO) {
+        return this.vendorTypeService.update(+vendor_type_id, updateVendorTypeDTO);
     }
 }
--- src/modules/master-data/vendor-type/vendor-type.service.ts
+++ src/modules/master-data/vendor-type/vendor-type.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateVendorTypeDTO } from './dto/create-vendor-type.dto';
+import { UpdateVendorTypeDTO } from './dto/update-vendor-type.dto';
 
 @Injectable()
 export class VendorTypeService {
@@ -26,6 +27,29 @@ export class VendorTypeService {
                 vendor_type_nameeng: createVendorTypeDTO.vendor_type_nameeng,
                 description: createVendorTypeDTO.description,
                 status: createVendorTypeDTO.status || "ACTIVE",
+            },
+            select: { // คืนค่าเฉพาะที่ใช้
+                vendor_type_id: true,
+                vendor_type_code: true,
+                vendor_type_name: true,
+                vendor_type_nameeng: true,
+                description: true,
+                status: true,
+            }
+        });
+    }
+
+    async update(vendor_type_id: number, updateVendorTypeDTO: UpdateVendorTypeDTO) {
+        return this.prisma.vendor_type.update({
+            where: {
+                vendor_type_id: vendor_type_id,
+            },
+            data: {
+                vendor_type_code: updateVendorTypeDTO.vendor_type_code,
+                vendor_type_name: updateVendorTypeDTO.vendor_type_name,
+                vendor_type_nameeng: updateVendorTypeDTO.vendor_type_nameeng,
+                description: updateVendorTypeDTO.description,
+                status: updateVendorTypeDTO.status || "ACTIVE",
             },
             select: { // คืนค่าเฉพาะที่ใช้
                 vendor_type_id: true,
