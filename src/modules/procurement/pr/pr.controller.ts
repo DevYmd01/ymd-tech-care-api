58643a4ec83d8b14637c5724ecc3362587033a08
Supakorn Muthita <supakorn.m@YMD.local>
2026-03-05 13:21:44 +0700

    3

--- src/modules/procurement/pr/pr.controller.ts
+++ src/modules/procurement/pr/pr.controller.ts
@@ -47,22 +47,22 @@ export class PrController {
         return this.PrService.update(+pr_id, dto, req.context);
     }
 
-    @Post(':pr_id/submit')
+    @Patch(':pr_id/submit')
     submit(@Param('pr_id') pr_id: number) {
         return this.PrService.submit(+pr_id);
     }
 
-    @Post(':pr_id/approve')
+    @Patch(':pr_id/approve')
     approve(@Param('pr_id') pr_id: number) {
         return this.PrService.approve(+pr_id);
     }
 
-    @Post(':pr_id/reject')
+    @Patch(':pr_id/reject')
     reject(@Param('pr_id') pr_id: number) {
         return this.PrService.reject(+pr_id);
     }
 
-    @Post(':pr_id/cancel')
+    @Patch(':pr_id/cancel')
     cancel(@Param('pr_id') pr_id: number) {
         return this.PrService.cancel(+pr_id);
     }
