9952b6e35c30c1747213215f6b50f68e86303368
DevYmd01 <itymdcenter@gmail.com>
2026-05-13 17:29:08 +0700

    module stock-balance

--- src/common/inventory/stock-balance/stock-balance.module.ts
+++ src/common/inventory/stock-balance/stock-balance.module.ts
@@ -2,8 +2,35 @@ import { Module } from '@nestjs/common';
 import { StockBalanceController } from './stock-balance.controller';
 import { StockBalanceService } from './stock-balance.service';
 
+// domain services
+import { ReceiveStockService } from './domain/service/movements/receive-stock.service';
+import { ReserveStockService } from './domain/service/movements/reserve-stock.service';
+import { ReleaseStockService } from './domain/service/movements/release-stock.service';
+import { IssueStockService } from './domain/service/movements/issue-stock.service';
+import { TransferStockService } from './domain/service/movements/transfer-stock.service';
+import { AdjustStockService } from './domain/service/movements/adjust-stock.service';
+
 @Module({
   controllers: [StockBalanceController],
-  providers: [StockBalanceService]
+  providers: [
+    StockBalanceService,
+
+    ReceiveStockService,
+    ReserveStockService,
+    ReleaseStockService,
+    IssueStockService,
+    TransferStockService,
+    AdjustStockService,
+  ],
+  exports: [
+    StockBalanceService,
+
+    ReceiveStockService,
+    ReserveStockService,
+    ReleaseStockService,
+    IssueStockService,
+    TransferStockService,
+    AdjustStockService,
+  ],
 })
 export class StockBalanceModule {}
